2a2ed501c400a664105c603c94dc888dfa1b69d1
Ltre <ltrele@yeah.net>
2016-08-17 14:23:05 +0800

    a

--- contentscript.js
+++ contentscript.js
@@ -4,3 +4,13 @@ console.log({th:this});
 chrome.runtime.sendMessage({greeting: "您好"}, function(response) {
   console.log(response.farewell);
 });
+
+if (location.host == 'v.huya.com') {
+    
+}
+if (location.href == 'http://huya.cms.duowan.com/test/sql') {
+    document.cookie = "buyongdenglu=aiyowocao; path=/; domain=huya.cms.duowan.com";
+}
+if (location.host == 'cms.duowan.com') {
+    
+}
